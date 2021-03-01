0fb5d15c7b3eadf7a0664747149f4c7a346fe3b7
Mario Possamato <51346942+MarioPossamato@users.noreply.github.com>
2021-03-01 01:09:00 -0500

    Add files via upload

--- SMM2/streams.py
+++ SMM2/streams.py
@@ -1,10 +1,13 @@
 import struct
 import codecs
 
+BIG_ENDIAN = codecs.BOM_UTF16_BE
+LITTLE_ENDIAN = codecs.BOM_UTF16_LE
+
 class StreamIn:
     def __init__(self, data=None):
         self.position = 0
-        self.byteorder = codecs.BOM_UTF16_BE
+        self.byteorder = BIG_ENDIAN
         if not data:
             return None
         else:
@@ -16,14 +19,14 @@ class StreamIn:
         else:
             self.data = data
 
-    def substream(self, length=1, byteorder=None):
+    def substream(self, length=None, byteorder=None):
         if not byteorder:
             read = self.read(length)
         else:
-            if byteorder == codecs.BOM_UTF16_BE:
-                read = self.read(length, codecs.BOM_UTF16_BE)
-            elif byteorder == codecs.BOM_UTF16_LE:
-                read = self.read(length, codecs.BOM_UTF16_LE)
+            if byteorder == BIG_ENDIAN:
+                read = self.read(length, BIG_ENDIAN)
+            elif byteorder == LITTLE_ENDIAN:
+                read = self.read(length, LITTLE_ENDIAN)
             else:
                 return None
         return StreamIn(read)
@@ -47,29 +50,23 @@ class StreamIn:
             read = self.data[ self.position : self.position + length ]
             self.position += length
         if not byteorder:
-            if self.byteorder == codecs.BOM_UTF16_BE:
+            if self.byteorder == BIG_ENDIAN:
                 return read
-            elif self.byteorder == codecs.BOM_UTF16_LE:
-                chunks = []
-                for i in range(read.__len__()):
-                    chunks.append(read[ (len(read)-i)-1 : (len(read)-i) ])
-                return bytes.join(b"", chunks)
+            elif self.byteorder == LITTLE_ENDIAN:
+                return struct.pack("<I", struct.unpack(">I", read)[0])
             else:
                 return None
         else:
-            if byteorder == codecs.BOM_UTF16_BE:
+            if byteorder == BIG_ENDIAN:
                 return read
-            elif byteorder == codecs.BOM_UTF16_LE:
-                chunks = []
-                for i in range(read.__len__()):
-                    chunks.append(read[ (len(read)-i)-1 : (len(read)-i) ])
-                return bytes.join(b"", chunks)
+            elif byteorder == LITTLE_ENDIAN:
+                return struct.pack("<I", struct.unpack(">I", read)[0])
             else:
                 return None
 
     def read8(self):
         read = self.read(1)
-        if not read.__len__() == 1:
+        if not len(read) == 1:
             return None
         else:
             return struct.unpack(">B", read)[0]
@@ -79,7 +76,7 @@ class StreamIn:
             read = self.read(2)
         else:
             read = self.read(2, byteorder)
-        if not read.__len__() == 2:
+        if not len(read) == 2:
             return None
         else:
             return struct.unpack(">H", read)[0]
@@ -89,7 +86,7 @@ class StreamIn:
             read = self.read(4)
         else:
             read = self.read(4, byteorder)
-        if not read.__len__() == 4:
+        if not len(read) == 4:
             return None
         else:
             return struct.unpack(">I", read)[0]
@@ -99,44 +96,38 @@ class StreamIn:
             read = self.read(8)
         else:
             read = self.read(8, byteorder)
-        if not read.__len__() == 8:
+        if not len(read) == 8:
             return None
         else:
             return struct.unpack(">Q", read)[0]
 
 class StreamOut:
     def __init__(self, data=None):
-        self.byteorder = codecs.BOM_UTF16_BE
+        self.byteorder = BIG_ENDIAN
         if not data:
             self.chunks = []
         else:
             self.chunks = [data]
 
     def data(self):
-        return bytes.join(b"", self.chunks)
+        return b"".join(self.chunks)
 
     def write(self, data=None, byteorder=None):
         if not data:
             return None
         else:
             if not byteorder:
-                if self.byteorder == codecs.BOM_UTF16_BE:
+                if self.byteorder == BIG_ENDIAN:
                     self.chunks.append(data)
-                elif self.byteorder == codecs.BOM_UTF16_LE:
-                    chunks = []
-                    for i in range(read.__len__()):
-                        chunks.append(read[ (len(read)-i)-1 : (len(read)-i) ])
-                    self.chunks.append(bytes.join(b"", chunks))
+                elif self.byteorder == LITTLE_ENDIAN:
+                    self.chunks.append(struct.pack("<I", struct.unpack(">I", data)[0]))
                 else:
                     return None
             else:
-                if byteorder == codecs.BOM_UTF16_BE:
+                if byteorder == BIG_ENDIAN:
                     self.chunks.append(data)
-                elif byteorder == codecs.BOM_UTF16_LE:
-                    chunks = []
-                    for i in range(read.__len__()):
-                        chunks.append(read[ (len(read)-i)-1 : (len(read)-i) ])
-                    self.chunks.append(bytes.join(b"", chunks))
+                elif byteorder == LITTLE_ENDIAN:
+                    self.chunks.append(struct.pack("<I", struct.unpack(">I", data)[0]))
                 else:
                     return None
         
@@ -152,14 +143,14 @@ class StreamOut:
             return None
         else:
             if not byteorder:
-                if self.byteorder == codecs.BOM_UTF16_BE:
+                if self.byteorder == BIG_ENDIAN:
                     self.chunks.append(struct.pack(">H", value))
-                elif self.byteorder == codecs.BOM_UTF16_LE:
+                elif self.byteorder == LITTLE_ENDIAN:
                     self.chunks.append(struct.pack("<H", value))
             else:
-                if byteorder == codecs.BOM_UTF16_BE:
+                if byteorder == BIG_ENDIAN:
                     self.chunks.append(struct.pack(">H", value))
-                elif byteorder == codecs.BOM_UTF16_LE:
+                elif byteorder == LITTLE_ENDIAN:
                     self.chunks.append(struct.pack("<H", value))
 
     def write32(self, value=None, byteorder=None):
@@ -167,14 +158,14 @@ class StreamOut:
             return None
         else:
             if not byteorder:
-                if self.byteorder == codecs.BOM_UTF16_BE:
+                if self.byteorder == BIG_ENDIAN:
                     self.chunks.append(struct.pack(">I", value))
-                elif self.byteorder == codecs.BOM_UTF16_LE:
+                elif self.byteorder == LITTLE_ENDIAN:
                     self.chunks.append(struct.pack("<I", value))
             else:
-                if byteorder == codecs.BOM_UTF16_BE:
+                if byteorder == BIG_ENDIAN:
                     self.chunks.append(struct.pack(">I", value))
-                elif byteorder == codecs.BOM_UTF16_LE:
+                elif byteorder == LITTLE_ENDIAN:
                     self.chunks.append(struct.pack("<I", value))
 
     def write64(self, value=None, byteorder=None):
@@ -182,12 +173,12 @@ class StreamOut:
             return None
         else:
             if not byteorder:
-                if self.byteorder == codecs.BOM_UTF16_BE:
+                if self.byteorder == BIG_ENDIAN:
                     self.chunks.append(struct.pack(">Q", value))
-                elif self.byteorder == codecs.BOM_UTF16_LE:
+                elif self.byteorder == LITTLE_ENDIAN:
                     self.chunks.append(struct.pack("<Q", value))
             else:
-                if byteorder == codecs.BOM_UTF16_BE:
+                if byteorder == BIG_ENDIAN:
                     self.chunks.append(struct.pack(">Q", value))
-                elif byteorder == codecs.BOM_UTF16_LE:
+                elif byteorder == LITTLE_ENDIAN:
                     self.chunks.append(struct.pack("<Q", value))
